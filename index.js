85cbbbe136fbad8d440736a7a8916c3da518540b
burroughclarke <burroughclarkesa@gmail.com>
2019-08-26 18:55:35 +0930

    Add files via upload

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('StopFalls Server')
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+// Access the parse results as request.body
+app.post('/', function(request, response){
+	console.log("request.body:");
+    console.log(request.body);
+
+    var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    console.log("client ip: " + ip)
+
+    response.send("data received.");
+});
