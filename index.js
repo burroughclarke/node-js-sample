204a2d6d3797d2bba12b4071605fe06662697e62
burroughclarke <burroughclarkesa@gmail.com>
2019-08-26 19:30:59 +0930

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('StopFalls Server')
+  response.send('StopFalls Server');
+  response.end();
 })
 
 app.listen(app.get('port'), function() {
