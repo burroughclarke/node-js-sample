b68f83f26303ccc5f547454fe8bc9947c674bb72
burroughclarke <burroughclarkesa@gmail.com>
2019-08-27 13:56:58 +0930

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var express    = require('express')
-var bodyParser = require('body-parser')
 
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-// parse application/json
-app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
