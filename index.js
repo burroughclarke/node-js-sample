bf5eb63a38fd0f163dcca64df2edceb0d0cf9386
Burrough Clarke <burroughclarkesa@gmail.com>
2019-09-01 12:50:45 +0930

    mongodb updated with response.body

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express    = require('express')
 
 var app = express()
 
+const mongo_uri = "mongodb+srv://burrough:mittens@stopfalls-neprh.mongodb.net/test?retryWrites=true&w=majority"
+
 app.set('port', (process.env.PORT || 5000))
 
 // heroku doesn't like bodyparser ...
@@ -41,8 +43,22 @@ app.post('/', function(request, response){
     var myJSON = { "name": "Chris", "age": "38" };
     response.send(myJSON);
 
+    MongoClient.connect(mongo_uri, function(err, db) {
+        console.log("POST: MongoDB connected");
+        if (err) throw err;
+        var dbo = db.db("stopfalls_db");
+        // var myobj = { name: "Company Inc", address: "Highway 37" };
+        var myobj = request.body;
+        dbo.collection("stopfalls_collection").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("POST: 1 document inserted");
+            db.close();
+        });
+    });
+
     // connection is taking far too long to close
     response.end();
+    // will not reach here
 });
 
 const MongoClient = require('mongodb').MongoClient;
@@ -50,13 +66,13 @@ const MongoClient = require('mongodb').MongoClient;
 // replace the uri string with your connection string.
 
 //           mongodb+srv://burrough:mittens@stopfalls-neprh.mongodb.net/test?retryWrites=true&w=majority
-const uri = "mongodb+srv://burrough:mittens@stopfalls-neprh.mongodb.net/test?retryWrites=true&w=majority"
-MongoClient.connect(uri, function(err, client) {
+MongoClient.connect(mongo_uri, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
    }
    console.log('MongoDB Connected...');
-   const collection = client.db("test").collection("devices");
+   const collection = client.db("stopfalls_db").collection("stopfalls_collection");
+
    // perform actions on the collection object
    
    client.close();
@@ -67,8 +83,13 @@ MongoClient.connect(uri, function(err, client) {
 
 // ############################
 
+// nodeJS-mongodb authorisation using JWTs:
+// https://medium.com/quick-code/handling-authentication-and-authorization-with-node-7f9548fedde8
+
+// mongodb has sample data in it: you should be able to pull data using 'get' first. 
+
 // send data to server:
-// curl --data "param1=value1&param2=value2" https://stopfalls.herokuapp.com/
+// curl -i -X POST -H "Content-Type: application/json" -d '{"userID":"1","time":"1567305876477","xAccel":"1.12345","yAccel":"2.12345","zAccel":"3.12345"}' http://localhost:5000
 
 // TODOs
 // – send android data to web app as JSON data
