feb3751be51dc372b6f8be62d97898f57adc3a71
Burrough Clarke <burroughclarkesa@gmail.com>
2019-11-07 16:28:09 +1030

    Update index.js

--- index.js
+++ index.js
@@ -175,6 +175,7 @@ app.get('/get_fall_data', function(request, response) {
 
   var qrcode = request.query.qrcode;
 
+
   console.log("retrieving activity data for qrcode [" + qrcode + "]");
 
   myqry = { "qrcode": qrcode};
@@ -187,7 +188,7 @@ app.get('/get_fall_data', function(request, response) {
       var dbo = db.db(stopfalls_db);
       // same query, just different collection
       dbo.collection("stopfalls_falls").find(myqry).forEach(function(doc) {
-          response_string_falls += "[fall timestamp=" + doc['timestamp'] + ", realFall=" + doc['timestamp'] + ", locationLat=" + doc['locationLat'] + ", locationLon=" + doc['locationLon'] + ", x=" + doc['accelX'] + ", y=" + doc['accelY'] + ", z=" + doc['accelZ'] + "]" + "\n"
+          response_string_falls += "[fall timestamp=" + doc['timestamp'] + ", realFall=" + doc['realFall'] + ", locationLat=" + doc['locationLat'] + ", locationLon=" + doc['locationLon'] + ", x=" + doc['accelX'] + ", y=" + doc['accelY'] + ", z=" + doc['accelZ'] + "]" + "\n"
           
           console.log(doc);
         }, function(err) {
